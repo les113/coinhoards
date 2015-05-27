ab6716a455cfac57b582e4cd828d80a07833dce3
les <les.lumb@it.ox.ac.uk>
2015-05-27 16:10:00 +0100

    fixed resp menu

--- inc/menu.php
+++ inc/menu.php
@@ -1,47 +1,37 @@
     <!-- menu -->   
-    <div class="container">
-        <div class="row topmenus">
-            <nav class="navbar mainnav">
-                <div class="navbar-header">
-                  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#Navbar">
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>                        
-                  </button>
-                </div>
-                <div class="collapse navbar-collapse" id="Navbar">
-                  <ul class="nav navbar-nav navbar-left">
-                    <li class="active"><a href="index.php">Home</a></li>
-                    <li><a href="search.php">Search</a></li>
-                    <li><a href="hoarddetail.php">Hoard Detail</a></li>
-                    <li><a href="coindetail.php">Coin Detail</a></li>
-                    <li><a href="typography.php">Typography</a></li>
-                    <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Drop Down<span class="caret"></span></a>
-                      <ul class="dropdown-menu">
-                        <li><a href="#">Submenu</a></li>
-                        <li><a href="#">Submenu</a></li>
-                        <li><a href="#">Submenu</a></li>
-                      </ul>
-                    </li>
-                  </ul>
-                </div>
-            </nav>
-			<!-- if required 
-			<nav class="navbar login">
-                <div class="navbar-header">
-                  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#Login">
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>                        
-                  </button>
-                </div>
-                <div class="collapse navbar-collapse" id="Login">
-                  <ul class="nav navbar-nav navbar-right">
-                    <li class="active"><a href="#">Login</a></li>
-                    <li><a href="#">Register</a></li>
-                  </ul>
-                </div>
-            </nav> -->
-			
-        </div> <!-- end container -->    
+<nav class="navbar navbar-default">
+  <div class="container">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
     </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="index.php">Home <span class="sr-only">(current)</span></a></li>
+        <li><a href="search.php">Search</a></li>
+        <li><a href="hoarddetail.php">Hoard</a></li>
+        <li><a href="coindetail.php">Coin</a></li>
+        <li><a href="typography.php">Typography</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
+          <ul class="dropdown-menu" role="menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+            <li class="divider"></li>
+            <li><a href="#">One more separated link</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
